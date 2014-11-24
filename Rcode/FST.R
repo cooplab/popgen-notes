b5a5edfc68c17e4588e321d7c65d5257c39ec907
cooplab <gcoop79@gmail.com>
2014-11-23 20:19:00 -0800

    added function to plot FST under split model

---
+++ Rcode/FST.R
@@ -0,0 +1,11 @@
+mu=1e-8;
+Time<-seq(1,8e4,length=100);
+N=1e4;
+theta= 4*N*mu;
+HB = 1-(1-mu)^(2*Time) + (1-mu)^(2*Time)*theta/(theta+1); 
+HS =theta/(theta+1)
+HT= (HB+HS)/2
+
+plot(Time,1-HS/HT,type="l",lwd=2,xlab="Split time, generations",ylab="FST = 1-HS/HT")
+
+lines(Time,Time/(4*N),col="red",lwd=2,lty=2)
