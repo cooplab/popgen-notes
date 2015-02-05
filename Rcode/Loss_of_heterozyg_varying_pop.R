25eb69e93f6979efeeb2c169157f82382463e6db
cooplab <gcoop79@gmail.com>
2015-02-04 16:52:56 -0800

    added code for varying repro. success and loss of het

--- Rcode/Loss_of_heterozyg_varying_pop.R
+++ Rcode/Loss_of_heterozyg_varying_pop.R
@@ -1,5 +1,11 @@
 
-N.vec<-c(rep(10,5),rep(3,2),rep(10,5))
+install.packages("plotrix")
+library("plotrix")
+#N.vec<-c(rep(20,5),rep(3,2),rep(20,5),rep(3,2),rep(20,5))
+const.RS<-FALSE
+
+N.vec<-rep(10,20)
+#N.vec<-c(rep(10,5),rep(3,2),rep(10,5))
 num.gens<-length(N.vec)
 offset<-0.1
 plot(c(1,num.gens),c(1,max(N.vec))+c(-offset,offset),type="n",axes=FALSE,xlab="",ylab="")
@@ -17,8 +23,16 @@ N.old<-N.vec[i]
 
  new.cols<-rep("black",2*N.new)
 
+if(const.RS){ 
+	repro.success<-rep(1/N.old,N.old)
+}else{
+	repro.success<-sample(c(rep(0.5/(N.old),N.old-2),c(0.25,0.25)),replace=FALSE)
+	}
+
+
+
  for(ind in 1:N.new){
-		par<-sample(1:N.old,2,replace=FALSE)
+		par<-sample(1:N.old,2,replace=FALSE,prob=repro.success)
 
 		which.allele<-sample(c(-1,1),1)
 		lines(c(i,i+1), c(par[1]+which.allele*offset,ind-offset),col="grey",lwd=0.5)
@@ -34,5 +48,5 @@ N.old<-N.vec[i]
  points(rep(i,N.old),1:N.old+offset, pch=19,cex=1,col=my.cols[(1:N)*2])
  points(rep(i,N.old),1:N.old-offset, pch=19,cex=1,col=my.cols[(1:N)*2-1])
  my.cols<-new.cols
-
+if(!const.RS) sapply(which(repro.success>1/N.old), function(ind){ draw.circle(x=i,y=ind,radius=0.3,nv=100,border=NULL,col=NA,lty=1,lwd=1)})
 }
