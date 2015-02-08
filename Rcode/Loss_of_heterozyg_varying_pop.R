4a44db7e4c7257d7b512bdcdd634d3a79a435328
cooplab <gcoop79@gmail.com>
2015-02-08 09:06:54 -0800

    moved ILS into Loss of het file

--- Rcode/Loss_of_heterozyg_varying_pop.R
+++ Rcode/Loss_of_heterozyg_varying_pop.R
@@ -2,7 +2,7 @@
 #install.packages("plotrix")
 #library("plotrix")
 
-simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.1, for.class= TRUE, initial.state="all.black",plot.freqs=FALSE){
+simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.1, for.class= TRUE, initial.state="all.black",plot.freqs=FALSE,mult.pop=FALSE,pops=FALSE){
    #  c(rep(10,5),rep(3,2),rep(10,5),rep(3,2),rep(10,5))  #
 	stopifnot(initial.state %in% c("all.black","all.diff","two.alleles") )
 
@@ -18,6 +18,16 @@ simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate
 		line.col<-"grey"
 		mut.line.lwd<-1
 		mut.line.col<-"grey"
+	}
+
+	num.gens<- length(N.vec)-1	
+
+	if(!mult.pop){
+		ind.pop.par<-matrix(1,nrow=max(N.vec),ncol=num.gens+1)
+		ind.pop<-matrix(1,nrow=max(N.vec),ncol=num.gens+1)
+	}else{
+			ind.pop.par<-pops[["ind.pop.par"]]
+			ind.pop<-pops[["ind.pop"]]
 	}
 	
 	num.gens<- length(N.vec)-1
@@ -41,7 +51,9 @@ simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate
 			
 		N.new<-N.vec[i+1]
 		N.old<-N.vec[i]
-		
+		points(rep(i,N.old),1:N.old+offset, pch=19,cex=1.3,col=my.cols[(1:N.old)*2])
+		points(rep(i,N.old),1:N.old-offset, pch=19,cex=1.3,col=my.cols[(1:N.old)*2-1])
+
 		 new.cols<-rep("black",2*N.new)
 		
 		if(const.RS){ 
@@ -49,11 +61,12 @@ simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate
 		}else{
 			repro.success<-sample(c(rep(0.5/(N.old),N.old-2),c(0.25,0.25)),replace=FALSE)
 			}
-		
-		
-		
+	
 		 for(ind in 1:N.new){
-				par<-sample(1:N.old,2,replace=FALSE,prob=repro.success)
+
+				this.pop.par <- ind.pop.par[ind,i+1]
+				available.pars <- (1:N.old)[which(ind.pop[1:N.old,i] == this.pop.par)]
+				par<-sample(available.pars,2,replace=FALSE,prob=repro.success[which(ind.pop[1:N.old,i] == this.pop.par)])
 		
 				which.allele.1<-sample(c(-1,1),1)
 				if(i != num.gens){ lines(c(i,i+1), c(par[1]+which.allele.1*offset,ind-offset),col=line.col,lwd=line.lwd)}
@@ -76,9 +89,9 @@ simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate
 				
 			}
 		}	
-		 
-		 points(rep(i,N.old),1:N.old+offset, pch=19,cex=1.3,col=my.cols[(1:N)*2])
-		 points(rep(i,N.old),1:N.old-offset, pch=19,cex=1.3,col=my.cols[(1:N)*2-1])
+		##redraw points to cover lines		 
+		points(rep(i,N.old),1:N.old+offset, pch=19,cex=1.3,col=my.cols[(1:N.old)*2])
+		points(rep(i,N.old),1:N.old-offset, pch=19,cex=1.3,col=my.cols[(1:N.old)*2-1])
 
 		 my.cols<-new.cols
 		 track.cols[[i+1]]<-my.cols
@@ -89,18 +102,21 @@ simulate.pop<-function(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate
 		plot(c(1,num.gens),c(0,1),type="n",axes=FALSE,xlab="",ylab="")
 		all.my.cols<-unique(unlist(track.cols))
 		my.col.freqs<-sapply(track.cols,function(my.gen){sapply(all.my.cols,function(my.col){sum(my.gen==my.col)})})
-		sapply(all.my.cols,function(col.name){lines(my.col.freqs[col.name,]/(2*N.vec),col=col.name)});
+		sapply(all.my.cols,function(col.name){lines(my.col.freqs[col.name,]/(2*N.vec),col=col.name,lwd=2)});
 		axis(2)
 	}
 }
 
 
-make.figures.for.class<-function(my.file="~/Dropbox/Courses/PBGG_Core/Popgen_teaching_Notes/Figs/drift_figs.pdf"){
 
-	single.crash<- c(rep(10,8),rep(3,2),rep(10,8))
+make.figures.for.class<-function(my.dir="~/Dropbox/Courses/PBGG_Core/Popgen_teaching_Notes/Figs/"){
+
+	single.crash<- c(rep(10,5),rep(3,2),rep(10,5))
 	repeated.crash<- c(rep(10,5),rep(3,2),rep(10,5),rep(3,2),rep(10,5))
 
-	pdf(file=my.file,width=10,height=5) #,width = 800, height = 400
+	pdf(file=paste(paste(my.dir,"drift_figs.pdf",sep=""),width=10,height=5) #,width = 800, height = 400
+
+###DEPICTIONS OF JUST DRIFT
 	simulate.pop(N.vec=rep(5,15), const.RS=TRUE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff")
 	simulate.pop(N.vec=rep(5,15), const.RS=TRUE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff")
 	simulate.pop(N.vec=rep(5,15), const.RS=TRUE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff")
@@ -121,6 +137,7 @@ make.figures.for.class<-function(my.file="~/Dropbox/Courses/PBGG_Core/Popgen_tea
 	simulate.pop(N.vec=rep(10,10), const.RS=FALSE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff")
 	simulate.pop(N.vec=rep(10,10), const.RS=FALSE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff")
 
+###MUTATION
 	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.1, for.class= TRUE, initial.state="all.black")
 	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.1, for.class= TRUE, initial.state="all.black")
 	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.1, for.class= TRUE, initial.state="all.black")
@@ -129,5 +146,28 @@ make.figures.for.class<-function(my.file="~/Dropbox/Courses/PBGG_Core/Popgen_tea
 	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.2, for.class= TRUE, initial.state="all.black")
 	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, mut.rate=  0.2, for.class= TRUE, initial.state="all.black")
 
+	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, for.class= TRUE,plot.freqs=TRUE,initial.state="all.black")
+	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, for.class= TRUE,plot.freqs=TRUE,initial.state="all.black")
+	simulate.pop(N.vec=rep(5,30), const.RS=TRUE,  mutation= TRUE, for.class= TRUE,plot.freqs=TRUE,initial.state="all.black")
+
    dev.off()
+
+	####Incomplete lineage sorting
+	one.out.of.3<-c(rep(1,5),rep(0,10))
+	two.out.of.3<-c(rep(1,5),rep(2,5),rep(0,5))
+	three.out.of.3<-c(rep(1,5),rep(2,5),rep(3,5))
+	pops<-list()
+	pops[["ind.pop.par"]]<-	matrix(c(rep(one.out.of.3,5),c(rep(1,10),rep(0,5)),rep(two.out.of.3,4),c(rep(1,5),rep(2,10)) ,rep(three.out.of.3,15)),nrow=15)	
+	N.vec<-apply(ind.pop.par,2,function(x){sum(x!=0)})
+	num.gens<- length(N.vec)-1
+    pops[["ind.pop"]]<-sapply(1:num.gens,function(i){c(rep(1,5),rep(2,5),rep(3,5))})
+   
+   	pdf(file=paste(my.dir,"ILS_figs.pdf",sep=""),width=15,height=8) #,width = 800, height = 400	
+	replicate(100,{
+		simulate.pop(N.vec=N.vec, const.RS=TRUE,  mutation= FALSE, for.class= TRUE, initial.state="all.diff",mult.pop=TRUE,pops=pops)
+		lines(x=c(6.5,100),y=c(5.5,5.5),lwd=4,col="darkgrey")	##show barrier to migration
+		lines(x=c(11.5,100),y=c(10.5,10.5),lwd=4,col="darkgrey")
+	})
+    dev.off() 			
+
 }
