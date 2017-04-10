3784d99b9b9c1606de8dcf7a2e00d98bd419376c
cooplab <gcoop79@gmail.com>
2017-04-10 12:33:07 -0700

    Added covar among rellys

--- Rcode/two_locus_sel.R
+++ Rcode/two_locus_sel.R
@@ -21,7 +21,7 @@ two.loc.sims<-function(p,w.mat,r){
 	return(cbind(p.array,d.array))
 }
 
-stack.freqs.plot<-function(p.out,my.order){
+stack.freqs.plot<-function(p.out){
 	stacked.freqs<-t(apply(p.out[,c("Ab","AB","ab","aB")],1,cumsum))
 	plot(stacked.freqs[,"Ab"],ylim=c(0,1),type="l",xlab="Generations",ylab="Frequencies",cex.lab=1.2)
 	my.x<-1:n.gens
@@ -33,18 +33,18 @@ stack.freqs.plot<-function(p.out,my.order){
 }
 
 
-stack.freqs.plot<-function(p.out,my.order=c("Ab","AB","aB","ab")){
-	cols<-c("white","blue","red","purple")
-	names(cols)<-c("ab","Ab","aB","purple")
-	stacked.freqs<-t(apply(p.out[,c("Ab","AB","ab","aB")],1,cumsum))
-	plot(stacked.freqs[,"Ab"],ylim=c(0,1),type="n",xlab="Generations",ylab="Frequencies",cex.lab=1.2)
-	my.x<-1:n.gens
-	x.polygon<-c(my.x,rev(my.x))
-	polygon(x=x.polygon,c(stacked.freqs[,my.order[1]],rep(0,n.gens)),col=cols[my.order[1]])
-	polygon(x=x.polygon,c(stacked.freqs[,my.order[2]],rev(stacked.freqs[,my.order[1]])),,col=cols[my.order[2]])
-	polygon(x=x.polygon,c(stacked.freqs[,my.order[2]],rev(stacked.freqs[,my.order[3]])),,col=cols[my.order[3]])
-	polygon(x=x.polygon,c(stacked.freqs[,my.order[3]],rev(stacked.freqs[,my.order[4]])),,col=cols[my.order[4]])
-}
+# stack.freqs.plot<-function(p.out,my.order=c("Ab","AB","aB","ab")){
+	# cols<-c("white","blue","red","purple")
+	# names(cols)<-c("ab","Ab","aB","purple")
+	# stacked.freqs<-t(apply(p.out[,c("Ab","AB","ab","aB")],1,cumsum))
+	# plot(stacked.freqs[,"Ab"],ylim=c(0,1),type="n",xlab="Generations",ylab="Frequencies",cex.lab=1.2)
+	# my.x<-1:n.gens
+	# x.polygon<-c(my.x,rev(my.x))
+	# polygon(x=x.polygon,c(stacked.freqs[,my.order[1]],rep(0,n.gens)),col=cols[my.order[1]])
+	# polygon(x=x.polygon,c(stacked.freqs[,my.order[2]],rev(stacked.freqs[,my.order[1]])),,col=cols[my.order[2]])
+	# polygon(x=x.polygon,c(stacked.freqs[,my.order[2]],rev(stacked.freqs[,my.order[3]])),,col=cols[my.order[3]])
+	# polygon(x=x.polygon,c(stacked.freqs[,my.order[3]],rev(stacked.freqs[,my.order[4]])),,col=cols[my.order[4]])
+# }
 
 
 ###Neutral Hitchhiking
