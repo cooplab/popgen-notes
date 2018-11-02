b7224b3613e361f82f5d4204421e47058ee6e5c6
Graham Coop <gmcoop@ucdavis.edu>
2018-11-02 16:16:08 -0700

    red deer

--- Journal_figs/Quant_gen/red_deer_selection_gradient/selection_grad_deer.R
+++ Journal_figs/Quant_gen/red_deer_selection_gradient/selection_grad_deer.R
@@ -7,4 +7,5 @@ red.deer[red.deer$LRS<0.5,"LRS"]<- 0. %%zeros from original log plot
 
   plot(red.deer,pch=19,cex.lab=1.4,cex.axis=1.3,main="",cex=1.2,xlab="Antler mass",ylab="Lifetime Reproductive Success")
 abline(lm(red.deer$LRS~red.deer$antler),col="red",lwd=2)
-dev.copy2pdf("Journal_figs/Quant_gen/red_deer_selection_gradient/selection_grad_deer.pdf")
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/red_deer_selection_gradient/selection_grad_deer.pdf")
+
