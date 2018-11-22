3d7848163055f1ba6f7903a91b6c305494d16400
Graham Coop <gmcoop@ucdavis.edu>
2018-11-22 13:04:49 -0800

    added Galtier figure

--- Journal_figs/drift_selection/DobPav/Dobzhansky_Pavlovsky.R
+++ Journal_figs/drift_selection/DobPav/Dobzhansky_Pavlovsky.R
@@ -5,7 +5,7 @@ large_DobPav<-DobPav[,grep("arge",colnames(DobPav))]
 small_DobPav<-DobPav[,grep("Small",colnames(DobPav))]
 
 
-par(mar=c(3,4.5,1,1))
+par(mar=c(3,4.5,1,1.5))
 layout(t(1:2))
 
 plot(c(1,3),c(0,50),type="n",axes=FALSE,xlab="",ylab="Allele Frequency",cex.lab=1.4)
@@ -19,3 +19,5 @@ axis(2,cex.axis=1.2)
 axis(1,at=1:3,lab=c("Start","4 months","6 months"),cex.axis=1.2)
 apply(small_DobPav,1,function(timepoints){lines(c(50,timepoints),type="b")})
 legend(x="bottomleft",leg="Small Populations",cex=1.4,bty="n")
+
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/DobPav/Drift_sel_Dobzhansky_Pavlovsky.pdf")
