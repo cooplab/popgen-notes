592b36d45c84a5ad91419d6962570d9c4f38e244
Graham Coop <gmcoop@ucdavis.edu>
2018-11-29 17:02:04 -0800

    Added bunch of weak drift and hitchiking

---
+++ Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.R
@@ -0,0 +1,12 @@
+codon.bias<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.csv")
+
+plot(codon.bias,pch=19,cex=1.5,xlab="Expression Level",ylab="Codon Bias",cex.lab=1.4,cex.axis=1.2)
+
+
+Leucine<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/Leucine_codon_bias.csv")
+par(mar=c(2,4,1,1))
+barplot(height=Leucine[Leucine$organism=="Drosophila","freq"],names.arg=Leucine[Leucine$organism=="Drosophila","codon"],ylab="Frequency",cex.lab=1.4,cex.axis=1.2)
+
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/Leucine_codon_bias.pdf")
+#,lab=barplot(Leucine[Leucine$organism=="Drosophila","codon"]))
+
