961a396cc19b95bcef1b0895911f6ae3897d4e23
Graham Coop <gmcoop@ucdavis.edu>
2018-12-03 06:30:12 -0800

    updated release

--- Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.R
+++ Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.R
@@ -2,6 +2,7 @@ codon.bias<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift
 
 plot(codon.bias,pch=19,cex=1.5,xlab="Expression Level",ylab="Codon Bias",cex.lab=1.4,cex.axis=1.2)
 
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/Drosophila_codon_bias_expression.pdf")
 
 Leucine<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/Leucine_codon_bias.csv")
 par(mar=c(2,4,1,1))
--- Rcode/two_locus_sel.R
+++ Rcode/two_locus_sel.R
@@ -87,6 +87,8 @@ layout(t(1:3))
  p.out<-two.loc.sims(p,w.mat,r=0.00005);stack.freqs.plot(p.out);
  p.out<-two.loc.sims(p,w.mat,r=0.0002);stack.freqs.plot(p.out);
 
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/figures/Deleterious_Hitchhiking.pdf")
+
 ##interference
 layout(t(1:2))
 n.gens<-1000
@@ -95,7 +97,7 @@ w.add<-c(1.14,1.08,1.06,1);names(w.add)<-names(p)
 w.mat<-outer(w.add,w.add,FUN="+")
 p.out<-two.loc.sims(p,w.mat,r=0.00)
 stack.freqs.plot(p.out)
-dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/figures/Deleterious_Hitchhiking.pdf")
+
 
 p<-  c(0.0,0.01,0.01,0.98); names(p)<-c("AB","Ab","aB","ab")
 w.add<-c(1.1,1.06,1.055,1);names(w.add)<-names(p)
@@ -124,3 +126,77 @@ p.out<-two.loc.sims(p,w.mat,r=0.1)
 stack.freqs.plot(p.out)
 
 dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/figures/synergistic_alleles_recom.pdf")
+
+
+my.fit.mat<-function(p){
+p_ab_ab<-outer(p,p)["ab","ab"]
+
+w_ab_ab<-dbeta(x=p_ab_ab,1,2)
+w_A_B<-dbeta(x=1-p_ab_ab,1,2)
+
+w.mat<-matrix(data=1,nrow=4,ncol=4,dimnames=list(names(p),names(p)))
+
+w.mat[grepl("A",rownames(w.mat)), grepl("B",colnames(w.mat)) ] <- w_A_B
+w.mat[grepl("B",rownames(w.mat)), grepl("A",colnames(w.mat)) ] <- w_A_B
+
+w.mat["ab","ab"]<-w_ab_ab
+w.mat
+}
+
+
+
+
+p<-  c(0.001,0.099,0,0.9); names(p)<-c("AB","Ab","aB","ab")
+w.add<-c(0.97,0.93,1,0.95);names(w.add)<-names(p)
+w.mat<-outer(w.add,w.add,FUN="+")
+
+
+my.fit.mat<-function(p){
+p_ab_ab<-outer(p,p)["ab","ab"]
+
+w_ab_ab<-dbeta(x=p_ab_ab,1,1.1)+0.3
+w_A_B<-dbeta(x=1-p_ab_ab,1,1.1)+0.3
+
+w.mat<-matrix(data=0.3,nrow=4,ncol=4,dimnames=list(names(p),names(p)))
+
+w.mat["AB",]<-w_A_B
+w.mat[,"AB"]<-w_A_B
+w.mat[grepl("A",rownames(w.mat)), grepl("B",colnames(w.mat)) ] <- w_A_B
+w.mat[grepl("B",rownames(w.mat)), grepl("A",colnames(w.mat)) ] <- w_A_B
+
+w.mat["ab","ab"]<-w_ab_ab
+w.mat
+}
+
+
+
+two.supergene.sims<-function(p,r){
+	stopifnot(sum(p)==1)
+	p.array<-matrix(NA,nrow=n.gens,ncol=4)
+	d.array<-NA
+	p.array[1,]<-p
+	colnames(p.array)<-names(p)
+	for(gen in 2:n.gens){
+		w.mat<-my.fit.mat(p)
+		w.marg<-apply(w.mat,1,function(whap){whap*p}) 
+		w.marg<-colSums(w.marg)
+		D<-p["AB"] - (p["AB"]+p["Ab"])*(p["AB"]+p["aB"])
+		d.array<-c(d.array,D)
+		D.vec<- c(-D,+D,+D,-D)
+		wbar<-sum(p*w.marg)
+		p.new<- (p*w.marg + r*D.vec*w.mat["AB","ab"])/wbar
+		p.array[gen,]<-p.new
+		p<-p.new
+	}
+	return(cbind(p.array,d.array))
+} 
+
+n.gens<-100
+layout((1:2))
+par(mar=c(4,4,0,0))
+p<-  c(0.25,0.25,0.25,0.25); names(p)<-c("AB","Ab","aB","ab")
+p.out<-two.supergene.sims(p,r=0.00); stack.freqs.plot(p.out); sum(p.out[90,1:3])
+p<-  c(0.25,0.25,0.25,0.25); names(p)<-c("AB","Ab","aB","ab")
+p.out<-two.supergene.sims(p,r=0.4); stack.freqs.plot(p.out); sum(p.out[90,1:3])
+
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/figures/supergene_recom.pdf")
