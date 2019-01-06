43528aeb974b07de915a55170f0565d068786a91
Graham Coop <gmcoop@ucdavis.edu>
2019-01-05 16:54:36 -0800

    added Pickrell code

---
+++ Journal_figs/Quant_gen/pickrell_pleiotropy/AAM_height_pickrell.R
@@ -0,0 +1,9 @@
+
+
+
+ AAM_Height<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/pickrell_pleiotropy/AAM_height_pickrell.csv")
+ plot(AAM_Height,pch=19,xlab="AAM effect size",ylab="Height effect size",cex.axis=1.2,cex.lab=1.4,cex=1.5)
+ 
+ 
+ dev.copy2pdf(file="Journal_figs/Quant_gen/pickrell_pleiotropy/AAM_height.pdf")
+ 
