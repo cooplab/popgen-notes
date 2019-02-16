afaa4cb62ecde8df250f6b97cd2a59d07d027c06
Graham Coop <gmcoop@ucdavis.edu>
2019-02-15 17:16:00 -0800

    added prob fix

--- Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.R
+++ Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.R
@@ -1,5 +1,5 @@
 codon.bias<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/codon_bias.csv")
-
+#data from  http://www.genetics.org/content/160/2/595
 plot(codon.bias,pch=19,cex=1.5,xlab="Expression Level",ylab="Codon Bias",cex.lab=1.4,cex.axis=1.2)
 
 dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/drift_selection/Codon_bias_Drosophila/Drosophila_codon_bias_expression.pdf")
