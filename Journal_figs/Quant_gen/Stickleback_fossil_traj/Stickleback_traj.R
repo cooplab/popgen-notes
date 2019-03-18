69876ffb1fdb0444ff1193b2e3ad2af4d7e8540c
Graham Coop <gmcoop@ucdavis.edu>
2019-03-17 18:42:04 -0700

    added stickleback

---
+++ Journal_figs/Quant_gen/Stickleback_fossil_traj/Stickleback_traj.R
@@ -0,0 +1,37 @@
+
+
+stickleback_traj<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Stickleback_fossil_traj/Touching_pterygiophores.csv")
+
+layout((1:2),height=c(0.8,0.3))
+par(mar=c(1,3.1,3.1,0.5))
+plot(stickleback_traj$touching, -stickleback_traj$gen,pch=19,axes=FALSE,type="b",xlim=c(0.6,1.42))
+axis(2,at=-c(0,2500,5000,7500),lab=c(0,2500,5000,7500)*2)
+axis(3)
+initial<-1.43
+opt<-0.82
+step<-8.5e-6
+alpha<-1.19e-3
+t<-stickleback_traj$generation
+expected.traj<-(1-exp(-alpha*t))*opt + exp(-alpha*t)*initial
+lines(expected.traj,-t)
+z<-seq(0.6,1.4,length=100)
+	mtext(side=3,"Touching Pterygiophores",cex=1.4,line=2)
+	mtext(side=2,"Years",cex=1.4,line=2)
+omega<-6.7
+fitness.surf<-exp(-(z-opt)^2/2*omega)
+fitness.traj<-exp(-(stickleback_traj$touching_pterygiophores-opt)^2/2*omega)
+
+par(mar=c(3.1,3.1,1,0.5))
+plot(z,fitness.surf,lwd=2,type="l",axes=FALSE,xlim=c(0.6,1.42)) #type="l",
+axis(1)
+axis(2)
+
+my.diff<-diff(fitness.traj) #stickleback_traj$touching)
+my.diff<-abs(my.diff)/max(abs(my.diff))
+for(i in 1:(nrow(stickleback_traj)-1)){ 
+	arrows(x0=stickleback_traj$touching[i],x1=stickleback_traj$touching[i+1],y0=fitness.traj[i],y1=fitness.traj[i+1],length=0.25*my.diff[i],col=adjustcolor("black",my.diff[i]),lwd=2)
+	}
+	mtext(side=2,"Fitness",cex=1.4,line=2)
+	mtext(side=1,"Touching Pterygiophores",cex=1.4,line=2)
+	
+	dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Stickleback_fossil_traj/Stickleback_fossil_traj.pdf")
--- Journal_figs/recom_selection/evening_primrose/evening_primrose_omega.R
+++ Journal_figs/recom_selection/evening_primrose/evening_primrose_omega.R
@@ -1,13 +1,13 @@
 
 primrose<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/recom_selection/evening_primrose/evening_primrose_omega.csv",as.is=TRUE,head=FALSE)
- my.cols<-c("blue","red");names(my.cols)<-c("sex","asex");
+my.pch<-c(19,18); names(my.pch)<-c("sex","asex");  #c("blue","red");
 
- plot(primrose$V4,pch=19,cex=2,col=my.cols[primrose$V2],cex.lab=1.4,cex.axis=1.2,xlab="",ylab="dN/dS",axes=FALSE)
+ plot(primrose$V4,cex=2,pch=my.pch[primrose$V2],cex.lab=1.4,cex.axis=1.2,xlab="",ylab="dN/dS",axes=FALSE)
  axis(side=2,cex.axis=1.2)
  for(i in 1:7){ 
  	abline(v=2*i+0.5,col="grey")
  	mtext(side=1,text=paste("C",i,sep=""),at=2*i-0.5,cex=1.4,line=1)
  	}
- legend(x="topright",legend=names(my.cols),pch=19,col=my.cols,cex=1.4,bg="white")
+ legend(x="topright",legend=names(my.pch),pch=my.pch,cex=1.4,bg="white")
  	
  dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/recom_selection/evening_primrose/evening_primrose_omega.pdf")
