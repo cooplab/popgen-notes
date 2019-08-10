6adf8f13702de85f0964218d27607a3405cf7e03
Graham Coop <gmcoop@ucdavis.edu>
2019-08-09 17:22:35 -0700

    updating LTS

--- Journal_figs/Quant_gen/Illinois_long_term_selection_corn/corn_LTS.R
+++ Journal_figs/Quant_gen/Illinois_long_term_selection_corn/corn_LTS.R
@@ -4,16 +4,68 @@
 #https://www.ideals.illinois.edu/handle/2142/3525
 
 corn_LTS<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Illinois_long_term_selection_corn/timmed_corn_LTS_table.csv",head=TRUE,as.is=TRUE,na.strings=".")
-
-plot(corn_LTS[,"YR"],corn_LTS[,"IHO.1"],type="l",ylim=c(0,25),lwd=2,xlab="Year",ylab="% Oil content",cex.axis=1.2,cex.lab=1.4)
-
+plot(corn_LTS[,"YR"],corn_LTS[,"IHO.1"],type="l",ylim=c(0,25),xlab="Year",ylab="% Oil content",cex.axis=1.2,cex.lab=1.4,lwd=2)
+abline(lm(corn_LTS[,"IHO.1"]~corn_LTS[,"YR"]),col="red",lwd=1.5)
 lines(corn_LTS[,"YR"],corn_LTS[,"ILO.1"],type="l",lty=2,lwd=2)
 
-lines(corn_LTS[,"YR"],corn_LTS[,"IRHO.1"],type="l",lty=3,lwd=3)
+lines(corn_LTS[,"YR"],corn_LTS[,"IRHO.1"],type="l",lty=3,lwd=2)
+lines(corn_LTS[,"YR"],corn_LTS[,"IRLO.1"],type="l",lty=4,lwd=2)
+dev.copy2pdf("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Illinois_long_term_selection_corn/Illinois_LTS_means.pdf")
 
 corn.ids<-read.csv(file="~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Illinois_long_term_selection_corn/LTINDIVIDUAL_EAR_DATA1896_2004A.csv",head=TRUE,as.is=TRUE,na.strings=".",comment.char = "#",skip=15)
 
-	my.breaks=seq(0,max(corn.ids[!is.na(corn.ids$IHO),"IHO"]),length=35)
+
+###ridge plot
+library("ggplot2")
+library("ggridges")
+
+ tmp1<-cbind(corn.ids[!is.na(corn.ids$IHO),c("Year","IHO")],"IHO"); colnames(tmp1)<-c("Year","Oil","Exp")
+ tmp2<-cbind(corn.ids[!is.na(corn.ids$ILO),c("Year","ILO")],"ILO"); colnames(tmp2)<-c("Year","Oil","Exp")
+tmp3<-cbind(corn.ids[!is.na(corn.ids$IRHO),c("Year","IRHO")],"IRHO"); colnames(tmp3)<-c("Year","Oil","Exp")
+tmp4<-cbind(corn.ids[!is.na(corn.ids$IRHO),c("Year","IRLO")],"IRLO"); colnames(tmp4)<-c("Year","Oil","Exp")
+
+pheno.sel<-rbind(tmp1,tmp2) #,tmp3,tmp4)
+pheno.sel$Exp<-as.factor(pheno.sel$Exp)
+
+
+ggplot(pheno.sel, aes(x = Oil, y = Year,group=paste(Year,Exp),  fill=Exp)) + 
+ #geom_point()
+geom_density_ridges(alpha = .5,scale = 10, size = 0.25, rel_min_height = 0.03)+
+  theme_ridges(grid = FALSE) +
+  # scale_y_reverse(breaks=c(2000, 1980, 1960, 1940, 1920, 1900), expand = c(0.01, 0))+
+  scale_x_continuous(limits=c(0, 25), expand = c(0.01, 0)) +
+ scale_fill_manual(values =c("#ff8080", "#8080ff"),labels = c("IHO","ILO")) +   #c("red","blue","purple"), labels = c("IHO", "ILO","IRHO"))+
+  labs(
+  x="Oil content (%)",
+  y="Year",
+  title="Illinois long term selection experiment"
+  )
+
+
+##Protein levels
+ tmp1<-cbind(corn.ids[!is.na(corn.ids$IHO),c("Year","IHP")],"IHP"); colnames(tmp1)<-c("Year","Prot","Exp")
+ tmp2<-cbind(corn.ids[!is.na(corn.ids$ILO),c("Year","ILP")],"ILP"); colnames(tmp2)<-c("Year","Prot","Exp")
+
+pheno.sel<-rbind(tmp1,tmp2) #,tmp3,tmp4)
+pheno.sel$Exp<-as.factor(pheno.sel$Exp)
+
+
+ggplot(pheno.sel, aes(x = Prot, y = Year,group=paste(Year,Exp),  fill=Exp)) + 
+ #geom_point()
+geom_density_ridges(alpha = .5,scale = 10, size = 0.25, rel_min_height = 0.03)+
+  theme_ridges(grid = FALSE) +
+  # scale_y_reverse(breaks=c(2000, 1980, 1960, 1940, 1920, 1900), expand = c(0.01, 0))+
+  scale_x_continuous(limits=c(0, 40), expand = c(0.01, 0)) +
+ scale_fill_manual(values =c("#ff8080", "#8080ff"),labels = c("IHP","ILP")) +   #c("red","blue","purple"), labels = c("IHO", "ILO","IRHO"))+
+  labs(
+  x="Protein",
+  y="Year",
+  title="Illinois long term selection experiment"
+  )
+
+
+###animation 
+my.breaks=seq(0,max(corn.ids[!is.na(corn.ids$IHO),"IHO"]),length=35)
 
 system("rm ~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Illinois_long_term_selection_corn/gif_pngs/*")
 gif.dir<-"~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/Illinois_long_term_selection_corn/gif_pngs/"
@@ -37,27 +89,12 @@ dev.off()
 system(paste("convert -delay 30 $(ls -v ", gif.dir, file.prefix,"*.png) ", gif.dir,file.prefix, "output.gif",sep=""))
 
 
- tmp1<-cbind(corn.ids[!is.na(corn.ids$IHO),c("Year","IHO")],"IHO"); colnames(tmp1)<-c("Year","Oil","Exp")
- tmp2<-cbind(corn.ids[!is.na(corn.ids$ILO),c("Year","ILO")],"ILO"); colnames(tmp2)<-c("Year","Oil","Exp")
-tmp3<-cbind(corn.ids[!is.na(corn.ids$IRHO),c("Year","IRHO")],"IRHO"); colnames(tmp3)<-c("Year","Oil","Exp")
-pheno.sel<-rbind(tmp1,tmp2) #,tmp3)
-pheno.sel$Exp<-as.factor(pheno.sel$Exp)
-
-
-ggplot(pheno.sel, aes(x = Oil, y = Year,group=paste(Year,Exp),  fill=Exp)) + 
- #geom_point()
-geom_density_ridges(alpha = .5,scale = 10, size = 0.25, rel_min_height = 0.03)+
-  theme_ridges() +
-   scale_y_reverse(breaks=c(2000, 1980, 1960, 1940, 1920, 1900), expand = c(0.01, 0))+
-  scale_x_continuous(limits=c(0, 25), expand = c(0.01, 0)) +
- scale_fill_manual(values =c("#ff8080", "#8080ff"),labels = c("IHO", "ILO")) +   #c("red","blue","purple"), labels = c("IHO", "ILO","IRHO"))+
-  labs(
-  x="Oil content (%)",
-  y="Year",
-  title="Illinois long term selection experiment"
-  )+
- theme_ridges(grid = FALSE)
-
- 
+##Variance 
+ plot(unique(corn.ids$Year), tapply(corn.ids$IHO,corn.ids$Year,var,na.rm=TRUE),ylim=c(0,4),col="red",pch=19,type="b",xlab="Year",ylab="Variance",cex.axis=1.2,cex.lab=1.4)
+ points(unique(corn.ids$Year), tapply(corn.ids$ILO,corn.ids$Year,var,na.rm=TRUE),ylim=c(0,4),col="blue",pch=19,type="b") 
+ points(unique(corn.ids$Year), tapply(corn.ids$IRHO,corn.ids$Year,var,na.rm=TRUE),ylim=c(0,4),col="purple",pch=19,type="b")
+ points(unique(corn.ids$Year), tapply(corn.ids$IRLO,corn.ids$Year,var,na.rm=TRUE),ylim=c(0,4),col="light blue",pch=19,type="b")
   #c("#D55E0050","#ff0000", "#0072B250")
   #scale_color_manual(values = c("#ff0000","#D55E00", "#0072B2"), guide = "none")
+
+
