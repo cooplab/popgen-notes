470bed661536b382fd7dadf95722219b3ec4d42f
Graham Coop <gmcoop@ucdavis.edu>
2019-08-20 12:40:59 -0700

    adding math background and straightening out response to seleciton chapter

---
+++ math_background/Calc_background.R
@@ -0,0 +1,91 @@
+library("latex2exp")
+
+  fx<-function(x){(-x^4/3 -(5/6)*x^3 +x)}
+ dy.dx<-function(x){ -4*x^3/3- (5/6)*3*x^2+1)}
+ d2y.dx2<-function(x){ -3*4*x^2/3- 2*(5/6)*3*x)}
+
+ x<-seq(-2,1,length=1000)
+peaks<-x[which(abs(dy.dx(x))<0.003)]
+btwn.peaks<-c(min(x),peaks)+ diff(c(min(x),peaks,max(x)))/2
+
+layout(1:2)
+ par(mar=c(2,4,1,1))
+
+ plot(x,fx(x),type="l",lwd=2,axes=FALSE,xlab="",ylab="")
+ mtext(at=btwn.peaks, side=1,c("A","B","C","D"),cex=1.3,line=1.5)
+ abline(h=0)
+  abline(v=peaks,lty=2,lwd=2)
+  axis(2,cex=1.2)
+#mtext("x",side=1,line=2.4,cex=1.4)
+mtext(TeX("$f(x)$"),side=2.4,line=2,cex=1.4)
+
+par(mar=c(4,4,1,1))
+
+ plot(x, dy.dx(x), lwd=2,type="l",axes=FALSE,xlab="",ylab="")
+ abline(h=0)
+ abline(v=peaks,lty=2,lwd=2)
+   axis(2,cex=1.2)
+#mtext("x",side=1,line=2,cex=1.4)
+mtext(TeX("$f'(x)$"),side=2.4,line=2,cex=1.4)
+axis(1,cex=1.2)
+mtext("x",side=1,line=2.5,cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Derivat.pdf")
+
+layout(1)
+par(mar=c(4,4,1,1))
+
+ plot(x, d2y.dx2(x), lwd=2,type="l",axes=FALSE,xlab="",ylab="")
+ abline(h=0)
+# abline(v=x[which(abs(dy.dx(x))<0.003)],lty=2,lwd=2)
+   axis(2,cex=1.2)
+    abline(v=peaks,lty=2,lwd=2)
+
+mtext(TeX("$f''(x)$"),side=2.4,line=2,cex=1.4)
+axis(1,cex=1.2)
+mtext("x",side=1,line=2.5,cex=1.4)
+
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/2nd_Derivat.pdf")
+
+plot.taylor.1<-function(a){
+	taylor.x<- seq(a-0.25,a+0.25,length=1000)
+	taylor.1<-fx(a) + (taylor.x-a) * dy.dx(a)
+	lines(taylor.x,taylor.1,col="red",lwd=2)
+	points(a,fx(a),pch=19,cex=1.4,col="red")
+}
+
+plot.taylor.2<-function(a){
+	taylor.x<- seq(a-0.25,a+0.25,length=1000)
+	taylor.2<-fx(a) + (taylor.x-a) * dy.dx(a) + (taylor.x-a)^2 * d2y.dx2(a)/2
+	lines(taylor.x,taylor.2,col="red",lwd=2)
+	points(a,fx(a),pch=19,cex=1.4,col="red")
+}
+
+par(mar=c(3.5,3.5,1,1))
+layout(1)
+ plot(x,fx(x),type="l",lwd=2,axes=FALSE,xlab="",ylab="",ylim=c(-0.7,0.45))
+ abline(h=0)
+  axis(2,cex=1.2)
+mtext("x",side=1,line=2,cex=1.4)
+mtext(TeX("$f(x)$"),side=2.4,line=2,cex=1.4)
+axis(1,cex=1.2)
+plot.taylor.1(-0.25)
+plot.taylor.1(-1.6)
+plot.taylor.1(0.4)
+plot.taylor.1(-1)
+plot.taylor.1(0.8)
+
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Taylor_1.pdf")
+par(mar=c(3.5,3.5,1,1))
+layout(1)
+ plot(x,fx(x),type="l",lwd=2,axes=FALSE,xlab="",ylab="",ylim=c(-0.7,0.45))
+ abline(h=0)
+  axis(2,cex=1.2)
+mtext("x",side=1,line=2,cex=1.4)
+mtext(TeX("$f(x)$"),side=2.4,line=2,cex=1.4)
+axis(1,cex=1.2)
+plot.taylor.2(-0.25)
+plot.taylor.2(-1.6)
+plot.taylor.2(0.4)
+plot.taylor.2(-1)
+plot.taylor.2(0.8)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Taylor_2.pdf")
--- math_background/Math-background.R
+++ math_background/Math-background.R
@@ -1,4 +1,17 @@
 library("RColorBrewer")
+library("latex2exp")
+
+
+
+library(VennDiagram)
+
+grid.newpage()
+draw.triple.venn(area1 = 22, area2 = 18, area3 = 13, n12 = 10, n23 = 0, n13 = 0, 
+    n123 = 0, category = c("Dog", "Cat", "Lizard"), lty = "blank", 
+    fill = c( "pink1","skyblue", "mediumorchid"),cex=1.4,cat.cex=1.4,lwd=3)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Venn_toys.pdf")
+    
+
 
 ##binomial
 my.cols<-brewer.pal(3,"Dark2")
@@ -30,15 +43,93 @@ mtext("i",side=1,line=2,cex=1.4)
 points(0:100,dbinom(x=0:100, size=100, prob=my.ps[2]),type="b",pch=my.pch[2],lwd=2,col=my.cols[2])
 
 points(0:100,dbinom(x=0:100, size=100, prob=my.ps[3]),type="b",pch=my.pch[3],lwd=2,col=my.cols[3])
-legend(x="topright",col=my.col,pch=my.pch,legend(paste("p = ",my.ps,sep=""),cex=1.4)
-
+legend(x="topright",col=my.cols,pch=my.pch,legend=paste("p = ",my.ps,sep=""),cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Binomial.pdf")
 ##Geometric
 
 
 par(mar=c(3,3.5,1,1))
-plot(1:21,dgeom(x=0:20, prob=0.5),type="b",pch=19,lwd=2,cex.axis=1.2,xlab="",ylab="")
-abline(v=1/0.5,col="grey",lwd=2)
-points(1:21,dgeom(x=0:20, prob=0.1),type="b",pch=22,lwd=2)
-abline(v=1/0.1,col="grey",lwd=2)
+my.cols<-brewer.pal(3,"Set1")
+my.pch<-c(19,21,22)
+my.ps<-c(0.9,0.5,0.1)
+plot(1:16,dgeom(x=0:15, prob=my.ps[1]),type="n",cex.axis=1.2,xlab="",ylab="")
+abline(v=1/my.ps[1],col="grey",lwd=2)
+abline(v=1/my.ps[2],col="grey",lwd=2)
+abline(v=1/my.ps[3],col="grey",lwd=2)
+points(1:21,dgeom(x=0:20, prob=my.ps[1]),type="b",pch=my.pch[1],col=my.cols[1],lwd=2)
+points(1:21,dgeom(x=0:20, prob=my.ps[2]),type="b",pch=my.pch[2],col=my.cols[2],lwd=2)
+points(1:21,dgeom(x=0:20, prob=my.ps[3]),type="b",pch=my.pch[3],lwd=2,,col=my.cols[3])
+legend(x="topright",col=my.cols,pch=my.pch,legend=paste("p = ",my.ps,sep=""),cex=1.4)
+
 mtext("Probability",side=2,line=2.4,cex=1.4)
 mtext("i",side=1,line=2,cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Geometric.pdf")
+
+
+par(mar=c(3,3.5,1,1))
+my.cols<-brewer.pal(3,"Set1")
+my.pch<-c(19,21,22)
+my.lambdas<-c(1,5,10)
+plot(0:15,dpois(x=0:15, lambda=my.lambdas[1]),type="n",cex.axis=1.2,xlab="",ylab="")
+abline(v=my.lambdas[1],col="grey",lwd=2)
+abline(v=my.lambdas[2],col="grey",lwd=2)
+abline(v=my.lambdas[3],col="grey",lwd=2)
+
+points(0:100,dbinom(x=0:100, size=100, prob=1/100),type="b",pch=my.pch[2],lwd=2,col=adjustcolor(my.cols[1],0.5))
+points(0:100,dbinom(x=0:100, size=100, prob=5/100),type="b",pch=my.pch[2],lwd=2,col=adjustcolor(my.cols[2],0.5))
+points(0:100,dbinom(x=0:100, size=100, prob=10/100),type="b",pch=my.pch[2],lwd=2,col=adjustcolor(my.cols[3],0.5))
+
+points(0:15,dpois(x=0:15, lambda=my.lambdas[1]),type="b",pch=my.pch[1],col=my.cols[1],lwd=2)
+points(0:15,dpois(x=0:15, lambda=my.lambdas[2]),type="b",pch=my.pch[2],col=my.cols[2],lwd=2)
+points(0:15,dpois(x=0:15, lambda=my.lambdas[3]),type="b",pch=my.pch[3],lwd=2,col=my.cols[3])
+#legend(x="topright",col=my.cols,pch=my.pch,legend=paste(TeX("$\\lambda = $"),my.lambdas,sep=""),cex=1.4)
+legend(x="topright",col=my.cols,pch=my.pch,legend=c(TeX("$\\lambda = 1$"),TeX("$\\lambda = 5$"),TeX("$\\lambda = 10$")),cex=1.4)
+
+mtext("Probability",side=2,line=2.4,cex=1.4)
+mtext("i",side=1,line=2,cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Poisson.pdf")
+
+
+
+library("MASS")
+layout(t(1:3))
+par(mar=c(4,4,0.5,0.1))
+X<-mvrnorm(n=40,mu=c(5,2),Sigma=matrix(c(1,0.0,0.0,1),byrow=TRUE,nrow=2))
+plot(X,pch=19,cex=1.5,xlab="",ylab="",cex.axis=1.2)
+mtext("Y",side=2,line=2.5,cex=1.4)
+mtext("X",side=1,line=2.5,cex=1.4)
+legend(x="topleft",legend=signif(cor(X)[1,2],2),cex=1.4)
+
+X<-mvrnorm(n=40,mu=c(5,2),Sigma=matrix(c(1,0.8,0.8,1),byrow=TRUE,nrow=2))
+print(cor(X))
+plot(X,pch=19,cex=1.5,xlab="",ylab="",cex.axis=1.2)
+mtext("X",side=1,line=2.5,cex=1.4)
+legend(x="topleft",legend=signif(cor(X)[1,2],2),cex=1.4)
+
+X<-mvrnorm(n=40,mu=c(5,2),Sigma=matrix(c(1,-0.6,-0.6,1),byrow=TRUE,nrow=2))
+print(cov(X))
+plot(X,pch=19,cex=1.5,xlab="",ylab="",cex.axis=1.2)
+mtext("X",side=1,line=2.5,cex=1.4)
+legend(x="topright",legend=signif(cor(X)[1,2],2),cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Covar.pdf")
+
+
+
+X<-mvrnorm(n=20,mu=c(5,2),Sigma=matrix(c(1,0.5,0.5,1),byrow=TRUE,nrow=2))
+print(cor(X))
+plot(X,pch=19,cex=1.5,xlab="",ylab="",cex.axis=1.2)
+mtext("X",side=1,line=2.5,cex=1.4)
+mtext("Y",side=2,line=2.5,cex=1.4)
+
+lm.model<-lm(X[,2]~X[,1])
+abline(lm.model,lwd=2)
+sapply(1:nrow(X),function(i){
+lines(rep(X[i,1],2), c(X[i,2],X[i,2]-lm.model$resid[i]),col="red",lwd=2,lty=3)
+}
+)
+abline(lm.model,lwd=2)
+points(X,pch=19,cex=1.5)
+my.nums<-signif(c(var(X[,1]),cov(X[,1],X[,2]), lm.model$coeff[2]),2)
+legend(x="bottomright",legend= paste(c("Var(X)=","Cov(X,Y)=","Slope="),my.nums),cex=1.4)
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/dist_pics/Linear_regression.pdf")
+
