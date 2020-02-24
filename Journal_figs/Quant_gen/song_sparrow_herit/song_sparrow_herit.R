6dd7a461debc3c835f083b941363cfc7cffad15e
Graham Coop <gmcoop@ucdavis.edu>
2020-02-24 15:23:20 -0800

    updated

--- Journal_figs/Quant_gen/song_sparrow_herit/song_sparrow_herit.R
+++ Journal_figs/Quant_gen/song_sparrow_herit/song_sparrow_herit.R
@@ -18,3 +18,15 @@ abline(lm.output,col="red",lwd=2)
 abline(0,1,col="grey")
 
 dev.copy2pdf(file="/Users/gcoop/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/song_sparrow_herit/song_sparrow_herit.pdf")
+
+
+#https://sci-hub.tw/https://www.jstor.org/stable/2408296?seq=1
+foster.sparrow<-read.csv("/Users/gcoop/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/song_sparrow_herit/song_sparrow_herit_foster.csv")
+layout(1)
+plot(foster.sparrow$foster_beak_depth,foster.sparrow$family_beak_depth,
+     pch=19,cex=1.4,asp=1, xlab="Mid-Foster Parent Beak Depth",ylab="Offspring Mean Beak Depth",cex.lab=1.4,cex.axis=1.2)
+lm.output<-lm(foster.sparrow$family_beak_depth~foster.sparrow$foster_beak_depth)
+legend(x="topleft",legend=paste("Slope=", format(lm.output$coefficients[2],dig=2)),cex=1.4,bty="n")
+abline(lm.output,col="red",lwd=2)
+abline(0,1,col="grey")
+dev.copy2pdf(file="/Users/gcoop/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/Quant_gen/song_sparrow_herit/song_sparrow_herit_foster.pdf")
