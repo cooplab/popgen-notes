06e06c04e7d532869729c325d3e7034012a4d6fc
Graham Coop <gmcoop@ucdavis.edu>
2020-08-04 08:40:21 -0700

    adding Scythian_horses

---
+++ Journal_figs/genetic_drift/Scythian_horses/Scythian_horses.r
@@ -0,0 +1,15 @@
+Scythian<-read.csv("~/Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/genetic_drift/Scythian_horses/Scythian_horses.csv",as.is=TRUE)
+
+Scythian<-Scythian[Scythian$pop %in% c("Berel","Nordic"),]
+
+my.bars<-barplot(pi~pop+locus, data=Scythian,beside=TRUE,cex.names=1.5,cex.lab=1.8,cex.axis=1.3,xlab="",ylab=expression(pi), 
+        legend.text = c("Scythian","Modern"),args.legend=list(cex=1.5)	
+)
+
+
+pi.ratios<-Scythian[Scythian$pop=="Nordic","pi"]/Scythian[Scythian$pop=="Berel","pi"]
+names(pi.ratios)<-Scythian$locus[1:4]
+pi.ratios<-pi.ratios[c("auto","mtDNA","X","Y")]
+text(x=my.bars[2,],y=Scythian[Scythian$pop=="Nordic","pi"][c(2,1,3,4)],format(pi.ratios,dig=2))
+dev.copy2pdf(file="Dropbox/Courses/Popgen_teaching_Notes/Journal_figs/genetic_drift/Scythian_horses/Scythian_horses.pdf")
+
--- math_background/Calc_background.R
+++ math_background/Calc_background.R
@@ -1,36 +1,48 @@
 library("latex2exp")
+library("RColorBrewer")
 
   fx<-function(x){(-x^4/3 -(5/6)*x^3 +x)}
- dy.dx<-function(x){ -4*x^3/3- (5/6)*3*x^2+1)}
- d2y.dx2<-function(x){ -3*4*x^2/3- 2*(5/6)*3*x)}
+ dy.dx<-function(x){ -4*x^3/3- (5/6)*3*x^2+1}
+ d2y.dx2<-function(x){ -3*4*x^2/3- 2*(5/6)*3*x}
 
  x<-seq(-2,1,length=1000)
 peaks<-x[which(abs(dy.dx(x))<0.003)]
 btwn.peaks<-c(min(x),peaks)+ diff(c(min(x),peaks,max(x)))/2
 
-layout(1:2)
- par(mar=c(2,4,1,1))
+###PLOT FUNCTION AND DERIVATIVE
+layout(1:3)
+par(mar=c(2,4,1,1))
 
- plot(x,fx(x),type="l",lwd=2,axes=FALSE,xlab="",ylab="")
- mtext(at=btwn.peaks, side=1,c("A","B","C","D"),cex=1.3,line=1.5)
- abline(h=0)
-  abline(v=peaks,lty=2,lwd=2)
-  axis(2,cex=1.2)
+plot(x,fx(x),type="l",lwd=2,axes=FALSE,xlab="",ylab="")
+mtext(at=btwn.peaks, side=1,c("A","B","C","D"),cex=1.3,line=1.5)
+abline(h=0)
+abline(v=peaks,lty=2,lwd=2)
+axis(2,cex=1.2)
 #mtext("x",side=1,line=2.4,cex=1.4)
 mtext(TeX("$f(x)$"),side=2.4,line=2,cex=1.4)
 
 par(mar=c(4,4,1,1))
 
- plot(x, dy.dx(x), lwd=2,type="l",axes=FALSE,xlab="",ylab="")
- abline(h=0)
- abline(v=peaks,lty=2,lwd=2)
-   axis(2,cex=1.2)
+plot(x, dy.dx(x), lwd=2,type="l",axes=FALSE,xlab="",ylab="")
+abline(h=0)
+abline(v=peaks,lty=2,lwd=2)
+axis(2,cex=1.2)
 #mtext("x",side=1,line=2,cex=1.4)
 mtext(TeX("$f'(x)$"),side=2.4,line=2,cex=1.4)
+plot(x, d2y.dx2(x), lwd=2,type="l",axes=FALSE,xlab="",ylab="")
+abline(h=0)
+# abline(v=x[which(abs(dy.dx(x))<0.003)],lty=2,lwd=2)
+axis(2,cex=1.2,at=c(-8,-6,-4,-2,0,2))
+abline(v=peaks,lty=2,lwd=2)
+
+mtext(TeX("$f''(x)$"),side=2.4,line=2,cex=1.4)
 axis(1,cex=1.2)
 mtext("x",side=1,line=2.5,cex=1.4)
-dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Derivat.pdf")
 
+dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Derivat_all_3.pdf")
+#dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Derivat.pdf")
+
+###PLOT SECOND DERIVATIVE AS ITS OWN PANEL, OLD
 layout(1)
 par(mar=c(4,4,1,1))
 
@@ -89,3 +101,25 @@ plot.taylor.2(0.4)
 plot.taylor.2(-1)
 plot.taylor.2(0.8)
 dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Taylor_2.pdf")
+
+my.cols<-brewer.pal("Dark2",n=3)
+y<-fx(x)
+plot(x,y,type="l",lwd=2,axes=FALSE,xlab="",ylab="")
+ these<-667:978
+ polygon(x=c(x[these],rev(x[these])),y=c(y[these],rep(0,length(these))),col=my.cols[2])
+ these<-500:667
+ polygon(x=c(x[these],rev(x[these])),y=c(y[these],rep(0,length(these))),col=my.cols[3])
+
+ these<-978:1000
+ polygon(x=c(x[these],rev(x[these])),y=c(y[these],rep(0,length(these))),col=my.cols[3])
+ abline(h=0)
+ 
+
+  axis(2,cex=1.2)
+ axis(1,cex=1.2)
+ mtext("x",side=1,line=2.5,cex=1.4)
+ mtext(TeX("$f(x)$"),side=2.4,line=2,cex=1.4)
+ 
+ dev.copy2pdf(file="~/Dropbox/Courses/Popgen_teaching_Notes/math_background/calc_pics/Integral_eg.pdf")
+ 
+ 
